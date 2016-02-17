e5efc0a46067f9a411b367faf6ef937319741a0b
longdt <longdt@localhost>
2016-02-17 15:20:05 +0700

    update user verifier test tool

--- src/signature-verifier.cpp
+++ src/signature-verifier.cpp
@@ -22,6 +22,11 @@ using namespace signverify;
 using namespace cv;
 using namespace std;
 
+int g_acceptCnt = 0;
+int g_faCnt = 0;
+int g_rejectCnt = 0;
+int g_frCnt = 0;
+
 void loadMUIDData(const path& p, long id, vector<Mat>& data, Mat& labels);
 
 void loadUData(ulong id, vector<Mat>& data, Mat& labels) {
@@ -35,38 +40,51 @@ void loadUData(ulong id, vector<Mat>& data, Mat& labels) {
 	loadMUIDData(p, id, data, labels);
 }
 
-int umain() {
-	UserVerifier verifier(lbpGrid);
+int testID(ulong id) {
+	UserVerifier verifier(hogGrid);
 	vector<Mat> data;
 	Mat labels;
-	ulong id = 18 ;
 	loadUData(id, data, labels);
 	verifier.train(data, labels);
-	//test
-	for (uint i = 0; i < data.size(); ++i) {
-		Mat sign = data[i];
-//		imshow("sign", sign);
-		cout << verifier.verify(sign, id) << "\texpected: " << labels.at<int>(0, i) << endl;
-//		waitKey(0);
-	}
-	cout << "test lan 2" <<endl;
-	data.clear();
-	labels = Mat();
-	loadUData(id, data, labels);
-	for (uint i = 0; i < data.size(); ++i) {
-		Mat sign = data[i];
-//		imshow("sign", sign);
-		cout << verifier.verify(sign, id) << "\texpected: " << labels.at<int>(0, i) << endl;
-//		waitKey(0);
-	}
-
-	cout << "test lan 3" <<endl;
+	int acceptCnt = 0;
+	int faCnt = 0;
+	int rejectCnt = 0;
+	int frCnt = 0;
 	path p("/home/thienlong/Downloads/Testdata_SigComp2011/SigComp11-Offlinetestset/Dutch/Questioned(1287)/0" + std::to_string(id));
 	for (auto&& file : directory_iterator(p)) {
 		Mat sign = imread(file.path().string(), 0);
 //		imshow("sign", sign);
 		float score = verifier.verify(sign, id);
+		if (file.path().filename().string().length() > 10) {
+			++rejectCnt;
+			++g_rejectCnt;
+			if (score > 0) {
+				++frCnt;
+				++g_frCnt;
+			}
+		} else {
+			++acceptCnt;
+			++g_acceptCnt;
+			if (score < 0) {
+				++faCnt;
+				++g_faCnt;
+			}
+		}
 		cout << file.path().string() << ":\t" << id << "\t" << score << endl;
 	}
+	cout << "FAR: " << faCnt / (float) acceptCnt << "\tFRR: " << frCnt / (float) rejectCnt << endl;
+}
 
+int main() {
+	path p("/home/thienlong/Downloads/Testdata_SigComp2011/SigComp11-Offlinetestset/Dutch/Reference(646)");
+	if (!exists(p) || !is_directory(p)) {
+		return 0;
+	}
+	for (auto&& x : directory_iterator(p)) {
+		string uid = x.path().filename().string();
+		ulong id = stol(uid);
+		testID(id);
+	}
+	cout << "TFAR: " << g_faCnt / (float) g_acceptCnt << "\tTFRR: " << g_frCnt / (float) g_rejectCnt << endl;
+	return 0;
 }
